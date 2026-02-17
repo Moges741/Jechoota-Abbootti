413a2bd077e1019da4ef03ddde6f63a49e2a260a
Moges741 <mogesse741@gmail.com>
2026-02-17 16:51:26 +0300

    added quotes for pray

--- DA/app/app.vue
+++ DA/app/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-
+<p class="text-green-400 text-center">Jechoota Abbootti</p>
   </div>
 </template>
---
+++ DA/app/components/Kadhannaa/kadhannaaQuotes.js
@@ -0,0 +1,94 @@
+// composables/kadhannaaQuotes.js
+
+export const kadhannaaQuotes = [
+  {
+    id: 1,
+    category: "Kadhannaa",
+    quote: "Waaqayyoof fuuliin urgaa gaarii qabu laphee Uumaa ishii galateeffattudha",
+    author: "Qul. Agnaaxiyoos"
+  },
+  {
+    id: 2,
+    category: "Kadhannaa",
+    quote: "Kadhannaaf yeroo siif mijate carraa argatteen kadhachuuf duubatti hin deebi'iin hin dhibaa'iinis kanarraa kan ka'e yerookee waan faayidaa hin qabnetti balleessuurra kadhannaan bu'aa hedduu argachuu dandeessa",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 3,
+    category: "Kadhannaa",
+    quote: "Namni yeroo kadhannaa dhiisetti meeshaa ittiin du'u qopheessuu eegala.",
+    author: "Qulqulloota Abbootii"
+  },
+  {
+    id: 4,
+    category: "Kadhannaa",
+    quote: "Namni hin kadhanne akka biyya daangaa hin qabneeti. Abbaan barbaade seenee ni saama innis kennaa isaaf kenname ni dhaba.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 5,
+    category: "Kadhannaa",
+    quote: "Daa'imummaarra namni darbe hundi fayyuuf kadhannaan dirqama, kunis barnoota Amantaati.",
+    author: "Filiiksiyus"
+  },
+  {
+    id: 6,
+    category: "Kadhannaa",
+    quote: "Halkan Waaqayyoon galateeffachuun uumame yeroo addaati.",
+    author: "Qul. Abbaa Yisihaaq"
+  },
+  {
+    id: 7,
+    category: "Kadhannaa",
+    quote: "Foon lubbuu hin qabne kan du'e akkuma ta'e, lubbuun kadhannaa hin taasisne kan duutedha.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 8,
+    category: "Kadhannaa",
+    quote: "Mucaan Abbaa isaa waliin akkuma haasa'u kadhannaan Waaqayyoo waliin haasa'i.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 9,
+    category: "Kadhannaa",
+    quote: "Waaqayyoo fuuldura kadhannaaf yeroo dhaabbattu akka barbada ibiddaa fuuldura dhaabbatteetti yaadi.",
+    author: "Barsiisaa Guddicha Yisihaaq"
+  },
+  {
+    id: 10,
+    category: "Kadhannaa",
+    quote: "Abbootiin keenya akkuma raawwatan rakkookee Waaqayyoo fuuldura keessee soomaafi kadhannaan furmaata argachuu ni dandeessa.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 11,
+    category: "Kadhannaa",
+    quote: "Aarsaan qulqullootaa imimmaan gaabbii dha.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 12,
+    category: "Kadhannaa",
+    quote: "Kadhannaan firii gaarummaafi dhabamuu dheekkamsaatis; kadhannaan yaadannoo gadda keessa keenyaati.",
+    author: "Abbaa Neeles"
+  },
+  {
+    id: 13,
+    category: "Kadhannaa",
+    quote: "Waantonni hundumtuu akka ati barbaadde akka ta'u hin hawwiin; akka Waaqayyoo itti gammaduutti malee. Akkas taanaan kan hin raafamneefi yeroo kadhannaakee hundatti Waaqayyoon kan galateeffattu ni taata.",
+    author: "Abbaa Neeles"
+  },
+  {
+    id: 14,
+    category: "Kadhannaa",
+    quote: "Yaa Uumaako fuuldura keetti kan hollatuufi humna kan hin qabne garbichakee anaan yaadadhu.",
+    author: "Abbaa Qeerloos"
+  },
+  {
+    id: 15,
+    category: "Kadhannaa",
+    quote: "Kan tuffatameefi kan dhiphate jechoota keetiin gargaari; hunda kan danda'u irree isaa mirgaan gargaaramtaatii kanneen rakkina keessa jiranis kadhannaakee yeroo hundaafi gadda lapheekeen hirmaadhu. Gaaffiwwankee hundaaf burqaa gaarummaarraa deebii ni argatta.",
+    author: "Qul. Eefreem"
+  }
+]
