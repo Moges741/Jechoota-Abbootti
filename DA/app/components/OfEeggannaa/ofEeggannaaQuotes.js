240c39caaeade5de86b6cfbd9f043b942c437abf
Moges741 <mogesse741@gmail.com>
2026-02-17 20:57:07 +0300

    added self control quotes

---
+++ DA/app/components/OfEeggannaa/ofEeggannaaQuotes.js
@@ -0,0 +1,45 @@
+
+export const ofEeggannaaQuotes = [
+  {
+    id: 1,
+    category: "Of Eeggannaa",
+    quote: "Namni tokko akkamitti akka qabachuu danda'u yoo hin beekne qabeenyi bofadha. Bofa of eeggannaa malee duubasaa yoo qaban harkatti maramee akkuma nama hiddu qabeenyas ogummaafi of eeggannaan yoo hin qabne akkanuma.",
+    author: "Qeleminxoos Isa Iskindiriyaa"
+  },
+  {
+    id: 2,
+    category: "Of Eeggannaa",
+    quote: "Namni lapheesaa yoo hin eegganne waan dhagahe hunda dagachuu eegala. Kana booda diinni isa keessatti bakka argata; garagalchees isa kuffisa akkuma zayitni guutee ifu jechuudha.",
+    author: "Abbaa Arsiyees"
+  },
+  {
+    id: 3,
+    category: "Of Eeggannaa",
+    quote: "Jireenyakee keessatti laphee akka keessummaa qabaadhu. Akka yeroo jalqabaaf itti dhiyaatteetti baay'ees itti hin dhiyaatiin; baay'ees irraa hin fagaatiin.",
+    author: "Abbaa Agaatoon"
+  },
+  {
+    id: 4,
+    category: "Of Eeggannaa",
+    quote: "Seexana caalaa bartee sodaadhu.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 5,
+    category: "Of Eeggannaa",
+    quote: "Cubbamtoota jaalladhu; hojiitiin garuu isaan hin fakkaatiin.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 6,
+    category: "Of Eeggannaa",
+    quote: "Qarshiin jiraachuunsaa beekame hir'achaafi dhumaa deema. Akkasumas hojiin gaarii namoota fuulduratti beekamaafi mul'achaa yeroo deemu; akkuma gagaa ibidda biraa baqaa deemuu ni hir'ata. Lubbuunis namoota fuulduratti kan galateeffamtu yoo ta'e ni dadhabdi, humni ishees ni dhuma.",
+    author: "Immaahooy"
+  },
+  {
+    id: 7,
+    category: "Of Eeggannaa",
+    quote: "Jalqabni cubbuufi bu'uurri isaa of eeggannaa dhabuudha.",
+    author: "Abbaa Pheemen"
+  }
+]
