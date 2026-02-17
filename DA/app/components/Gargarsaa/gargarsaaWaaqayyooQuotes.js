3469150df3ec8b6812216e02b18c984c53d2c263
Moges741 <mogesse741@gmail.com>
2026-02-17 21:14:57 +0300

    added help of God quotes

---
+++ DA/app/components/Gargarsaa/gargarsaaWaaqayyooQuotes.js
@@ -0,0 +1,27 @@
+
+export const mootiiQuotes = [
+  {
+    id: 1,
+    category: "Gargarsaa Waaqayyoo",
+    quote: "Nama waliin yoo wallolte yookiin ni moota yookiin ni moo'amta. Mana kiristaanaa garuu moo'achuun hin danda'amu. Manni kiristaanaa yeroo hundumaa waraanatti jirti garuu moo'amtee hin beektu. Diyaabiloos hidhannoosaa ni hiike; meeshaa waraanaa isaas xumure M/Kiristaanaa garuu hin miine.",
+    author: "Unknown"
+  },
+  {
+    id: 2,
+    category: "Gargarsaa Waaqayyoo",
+    quote: "Hoolaa Isa bade Addaamiin barbaadee yeroo Isa argettis kan baate arjaa eegaa keenya Kiristoos dha.",
+    author: "Raajii Maariyaamii"
+  },
+  {
+    id: 3,
+    category: "Gargarsaa Waaqayyoo",
+    quote: "Osoo hin dadhabiin hundumaa ni baatta. Osoo hin hir'isiin hundumaa ni nyaachista. Osoo hin dagatiin hundumaa ni yaadatta. Osoo hin rafiin hunda ni dhageessa. Osoo haaloo hin bahiin hundaaf dhiifama taasista.",
+    author: "Qul. Yohannis"
+  },
+  {
+    id: 4,
+    category: "Gargarsaa Waaqayyoo",
+    quote: "Gara moottiitti nama dhiyaate eenyuyyuu argachuufi miidhuu akkuma hin dandeenye. Gara Waaqayyootti nama dhiyaate Diyaabiloos homaa gochuu hin danda'u. Garuu yeroo baay'ee waan ol ol ofqabnuufi dadhabbii keenya waan hin beekneef diinni nuutiin gara badiitti nugatuufi gara wantota qaanessaa ta'anitti nugeessuuf hin rakkatu.",
+    author: "Abbaa Tokko"
+  }
+]
