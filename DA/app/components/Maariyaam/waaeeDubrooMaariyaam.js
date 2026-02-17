8c8239b84a7d16b2709804cd3ab427c85e7692a9
Moges741 <mogesse741@gmail.com>
2026-02-17 21:12:29 +0300

    added Merry related quotes

---
+++ DA/app/components/Maariyaam/waaeeDubrooMaariyaam.js
@@ -0,0 +1,33 @@
+
+export const waaeeDubrooMaariyaamQuotes = [
+  {
+    id: 1,
+    category: "Waa'ee Dubroo Maariyaam",
+    quote: "Waaqni intala mooticha heeroodsi irraa osoo dhalateera ta'ee mootii kan ta'uufi eenyuyyuu Isa hin godaansisu ture.",
+    author: "Abbaa Giyoorgis Isa Gaasiccaa"
+  },
+  {
+    id: 2,
+    category: "Waa'ee Dubroo Maariyaam",
+    quote: "Yaa tulluu guddichaa mucayyoon xiqqoon kan sibaatte ofiikee hanga dandeettiisheetti gadi qabdeetu; Yaa Ibidda guddichaa mucayyoon xiqqoon kan hin gubanne humna isaa akka dandeessuuf ishee cimsiteetu.",
+    author: "Qul. Eephreem Isa Sooriyaa"
+  },
+  {
+    id: 3,
+    category: "Waa'ee Dubroo Maariyaam",
+    quote: "Addunyaan kun Ilma Waaqayyoo kallattiin Waaqa Abbaa irraa fudhachuuf kan isaaf hin malle ta'ee waan argameef Addunyaan ishee irraa akka fudhatuuf Tokkicha mucaa isaa Durbee Maariyaamiif haadha akka taatuuf kenneef.",
+    author: "Qul. Awugisxiinos"
+  },
+  {
+    id: 4,
+    category: "Waa'ee Dubroo Maariyaam",
+    quote: "Heeroodsi kan hin qabanne qabuuf ka'e qilleensas Mana isatti galchee itti cufuuf dhama'e.",
+    author: "Abbaa Giyoorgis Isa Gaasiccaa"
+  },
+  {
+    id: 5,
+    category: "Waa'ee Dubroo Maariyaam",
+    quote: "Yaa gooftaa haadhakee eenyu jennee waamnu? Haadha akka hin jenne Durba taatee argineen, durba akka hin jenne mucaa hammattee argineen. Yaa gooftaa haadhakee waamuuf akkana kan rakkisu yoo ta'e siitiinimmoo eenyu jennee waamna?",
+    author: "Qul. Eephreem"
+  }
+]
