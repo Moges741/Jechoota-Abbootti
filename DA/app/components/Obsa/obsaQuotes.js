fede7f0f9bd6005e36fd505693bd62559ca85672
Moges741 <mogesse741@gmail.com>
2026-02-17 23:14:10 +0300

    added patience quotes

---
+++ DA/app/components/Obsa/obsaQuotes.js
@@ -0,0 +1,45 @@
+
+export const obsaQuotes = [
+  {
+    id: 1,
+    category: "Obsa",
+    quote: "Lola, ariitiifi muddama irraa of to'adhu. Daftee hin dubbatiin; daftees yaadakee hin tarreessiin. Namoonni kaan yeroo dubbatanis gidduu hin galiin. Wanta itti hin amannerrattis murtee hin kenniin.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 2,
+    category: "Obsa",
+    quote: "Dhiphina danda'uun qulqullummaa guddaadha. Dhiphina keessatti gammaduun immoo qulqulluummaa caaludha.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 3,
+    category: "Obsa",
+    quote: "Hammenyaafi dogongoraan namni kamiyyuu yoo sitti ori'e dogongora isaa obsaan fudhadhu malee atis akka isaa ta'uun daawwitii hin ta'iiniif.",
+    author: "Baasiliyuus Isa Qiisaariyaa"
+  },
+  {
+    id: 4,
+    category: "Obsa",
+    quote: "Foon keenya akka Mana Qulqullummaa Gooftaatti eeggachuu qabna.",
+    author: "Qul. Qelemenxoos Isa Room"
+  },
+  {
+    id: 5,
+    category: "Obsa",
+    quote: "Qormaanni irra deddeebiin yoo sin qunname yaadda'uufi aaruu hin qabdani. Kana irra dhuma isaa obsaan eegaa; rakkinawwan isin mudatu galataan yoo fudhattan dhumni isaa shakkii tokko malee kennaa Waaqayyoo kan isiniif argamsiisudha.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 6,
+    category: "Obsa",
+    quote: "Obsi sammuu fageessee yaadu fi galma guddaa kan qabu irraa kan argamudha.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 7,
+    category: "Obsa",
+    quote: "Warqeen abiddaan akkuma hin badne namni obsa qabus qormaanni isa hin balleessu. Ibidda warqee maal godha? Ni miidhagsa malee. Nama obsa qabus qormaanni maal taasisa? Kabajamaafi kan ol ol jedhe isa taasisa malee.",
+    author: "Qul. Yaareed"
+  }
+]
