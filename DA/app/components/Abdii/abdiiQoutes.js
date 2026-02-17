63e03962287e1b9aadc612d062da1d251ef132f9
Moges741 <mogesse741@gmail.com>
2026-02-17 20:52:27 +0300

    added hope quotes

---
+++ DA/app/components/Abdii/abdiiQoutes.js
@@ -0,0 +1,57 @@
+
+export const abdiiQuotes = [
+  {
+    id: 1,
+    category: "Abdii",
+    quote: "Abdii malee jireenya dhabnee yeroo jiraannu bara xumuraatti Gooftaan keenya jireenya keenya nuuf deebiseeraatii Isa galateeffanna.",
+    author: "Tiksee Heermaan"
+  },
+  {
+    id: 2,
+    category: "Abdii",
+    quote: "Firii mukaa nyaachuun sababa ajajamuu dhabuun du'a nutti dhufe; Iyyesuus Kiristoos ajajamuun muka fannoorratti fannifamuun du'a balleessee jireenya nuuf kenne.",
+    author: "Qul. Heereenewoos"
+  },
+  {
+    id: 3,
+    category: "Abdii",
+    quote: "Waqtii kamittittiyyuu manni Amantaa yeroo sin ergitu moo'amtanii hin galiinaa.",
+    author: "Abbaa Inniyew"
+  },
+  {
+    id: 4,
+    category: "Abdii",
+    quote: "Jireenyi afuuraakeen hangamiyyuu dadhabaa yoo taate abdii hin kutatiin; abdii kutuun Diyaabiloos harka isaa irratti akka kuftuuf hamileekee kan itti dadhabsuufi dandeettii kee kan itti balleessu meeshaa waraanaa seexanaa isa guddaadha.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 5,
+    category: "Abdii",
+    quote: "Qulqulluu kan ta'e Waaqayyo abdii yeroo kennu nan gowomsama ta'aa jedhee shakkuun eenyuuf danda'ama? Nuuf kennuu osoo hin barbaadu ta'ee nakadhadhaa nun hin jedhu ture.",
+    author: "Awugisxiinoos"
+  },
+  {
+    id: 6,
+    category: "Abdii",
+    quote: "Ani abdii kan dhabeef, abdii gargaarsa kan dhabeef, gargaaraa dhukkubsataaf fayyina kan hin qabneef du'aa ka'umsa jedha Gooftaan keenya Iyyesuus Kiristoos.",
+    author: "Qul. Toomaas"
+  },
+  {
+    id: 7,
+    category: "Abdii",
+    quote: "Addunyaa kana irratti abdii dhabuun jalqaba fayyinakee ta'uu danda'a; kan ati fayyitu garuu abdii dhabuukeetiin osoo hin taane arjummaa Waaqayyootiini.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 8,
+    category: "Abdii",
+    quote: "Namni miila hin qabneefi namni abdii hin qabne bakka yaade gahuu hin danda'u.",
+    author: "Haayile Giyorgis"
+  },
+  {
+    id: 9,
+    category: "Abdii",
+    quote: "Dhalootni ammaa daandii fooyya'aarra deemuu kan danda'u kaleessaa abboonni isaanii daandii irra deeman yeroo beekan qofadha. Waa'ee borii yeroo yaadnutti waa'ee kaleessaa beekuu qabna; kaleessi Kitaaba irraa barannudha waan ta'eef.",
+    author: "Abbaa Phaawuloos"
+  }
+]
