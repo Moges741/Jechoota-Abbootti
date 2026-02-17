3067fd16b4a3db27193b4d211612cce13759bcc6
Moges741 <mogesse741@gmail.com>
2026-02-17 20:18:30 +0300

    added fasting quotes

---
+++ DA/app/components/Sooma/soomaQuotes.js
@@ -0,0 +1,87 @@
+
+export const soomaQuotes = [
+  {
+    id: 1,
+    category: "Sooma",
+    quote: "Hojii humna keessaniin olii hojjechuun ta'uu danda'a. Qulqulloonni kana ilaalchisee akkana jedhu: hojiin itti fufaa xiqqoon yeroo xiqqoo booda kan dhaabbatu hojii guddaarra ni caala.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 2,
+    category: "Sooma",
+    quote: "Addunyaa kana yaaduun yeroo hunda nurratti aangoo akka hin qabaanneef gaachana jabaa kan ta’ee sooma yaa qabannu.",
+    author: "Abune Shiinoodaa"
+  },
+  {
+    id: 3,
+    category: "Sooma",
+    quote: "Soomni gaachanadha: namni kamiyyuu humna daabiloos sooman injifachuu ni danda’a.",
+    author: "Qulqulluu Indiriiyaas"
+  },
+  {
+    id: 4,
+    category: "Sooma",
+    quote: "Namni tokko magaala diinota isaa too’achuu yoo barbaade dursa kan raawwachuu qabu daandii nyaata fi dhugaatii gara magaalichaa seenuu guutumaa guututti cufuudha. Foon keenyas akkasuma. Namni yommuu soomu baay’ee waan beela’uuf diinonni lubbuu isaa ariitiin dadhabu.",
+    author: "Qulqulluu Yohaannis Hatsiir"
+  },
+  {
+    id: 5,
+    category: "Sooma",
+    quote: "Namni nyaata fi dhugaatirraa of eeguun soomu maqaa namaa balleessuun foon namaa nyaata yoo ta’ee soomni kun sooma dhibaa’ootaadha.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 6,
+    category: "Sooma",
+    quote: "Qormaata nyaata fi dhugaatiin dhufu sodaachuu qabna; nyaanni, dhugaatiin fi quufni jalqaba hojii hamaa akkuma ta’ee, baay’inni hirribaas ejjummaa baay’isa.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 7,
+    category: "Sooma",
+    quote: "Namni sooma jibbee fi tuffate waan gaarii hunda ofirraa fageesse.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 8,
+    category: "Sooma",
+    quote: "Soomni kadhannaa, madda boo’ichaa fi gaabbii, obsa kan nama barsiistudha.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 9,
+    category: "Sooma",
+    quote: "Soomni madaa lubbuu fayyisti; dargaggootaas obsaan jiraachuu barsiisti.",
+    author: "Qulqulluu Yaareed"
+  },
+  {
+    id: 10,
+    category: "Sooma",
+    quote: "Yeroo hunda kan soomtu yoo ta’e altokkotti ol of hin kaasin; of hin tuulin. Soomarraa kan ka’ee ofiikeen of guddistee kan ilaaltu yoo ta’ee garuu foon yoo nyaatte siif wayya. Nama tokkoof of tuulummaan of guddisuurra fi of kabajuurra yoo foon nyaate wayyaaf.",
+    author: "Abbaa Eesdiiroos"
+  },
+  {
+    id: 11,
+    category: "Sooma",
+    quote: "Soomni amala bineensummaa kan nurra mulqu ergamoota fakkaannee akka jiraannu kan nu taasisu seera gaariidha. Soomarra jaalalatu caala.",
+    author: "Gedle Tekle Haayimaanot"
+  },
+  {
+    id: 12,
+    category: "Sooma",
+    quote: "Tsoomiin qoricha, qoricha ittiin dhukkuba lubbuu keenyaa irraa fayyinuudha. Akkaataa fudhannaa qoricha kanaa sirritti yoo hin beekne garuu fayidaa hin qabu. Qoricha kana namni fudhatu sa’atii meeqatti akka fudhatu, hammam akka liqimsu, dhukkuba gosa kamiif akka fayyaddu, yeroo hammamii fi haala qilleensaa akkamii keessatti akka fudhatu, ishee waliin gosoota nyaataa nyaatamuu qabanii fi hin qabne akkasumas wantoota ishee waliin hin deemne adda baasee beekuu qaba.",
+    author: "Qulqulluu Yohaannis Afaan Warqee"
+  },
+  {
+    id: 13,
+    category: "Sooma",
+    quote: "Tsoomaa jirtuu? Egaa tsooma keessan gochaan natti agarsiisa. Harka qalleessi gargaarsa keessan barbaade yoo ta’e gaarii taasisaafi; nama jibbitan yoo agartan dafaatti dhiifama taasisaafi; babbareeddonni yeroo karaa irra deeman ilaaltanii ejja hin hawwinaa. Afaan keessan qofa osoo hin taane, iji keessan, milli keessan, harki keessan, walumagalatti qaamonni miiraa keessan hunduu tsoomuu qabu.",
+    author: "Qulqulluu Yohaannis Afaan Warqee"
+  },
+  {
+    id: 14,
+    category: "Sooma",
+    quote: "Waqtii tsoomii keessanittis ta’e yeroo hundumaa waan sadii sammuutti akka qabattaanin isin gorsa: waan hamaa dubbachuu irraa of eeggachuu, nama kaminiyyuu akka diinaatti ilaaluu irraa of eeggachuu fi kakuu akka barsiifataatti qabattan irraa fagaachuu.",
+    author: "Qulqulluu Yohaannis Afaan Warqee"
+  }
+]
