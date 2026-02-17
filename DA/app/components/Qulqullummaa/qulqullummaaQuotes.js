8401aebf19d125af98a9d9d74c05159ae5328c56
Moges741 <mogesse741@gmail.com>
2026-02-17 20:49:59 +0300

    added righteousness quotes

---
+++ DA/app/components/Qulqullummaa/qulqullummaaQuotes.js
@@ -0,0 +1,39 @@
+
+export const qulqullummaaQuotes = [
+  {
+    id: 1,
+    category: "Qulqullummaa",
+    quote: "Ofii keenya akka yakkamaa taasisnee ofitti kan murteessinu yoo ta'e namoota kaan qulqulluu fi ergamootaa ta'anii isaan argina; garuu namoota kaan irratti akka yakkamaatti itti murteessina taanaan ofii keenya akka qulqulluutti of ilaalla.",
+    author: "Qul. Atnaatewoos"
+  },
+  {
+    id: 2,
+    category: "Qulqullummaa",
+    quote: "Kiristoos fuuldura yeroo kadhannaaf dhaabbattu akka hin qaanofne hojiikee kam keessatti qulqullummaa qabaadhu.",
+    author: "Unknown"
+  },
+  {
+    id: 3,
+    category: "Qulqullummaa",
+    quote: "Laphee qulqulluu akka qabaattuuf namoota kaan xureessuufi hamachuurraa of ittisi. Dubbii nama hadheessuu gurri dhagahu tasumaa lapheesaa osoo hin xureessiin qulqullummaadhaan eeguu hin danda'u.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 4,
+    category: "Qulqullummaa",
+    quote: "Akka suraafeel qulqulluummaa qaamni qabate maqaansaa haa galatoomuuti, Waaqayyo biraatii irra deddeebiin kan dhagahamu aarsaa qulqulluurra kan kabajamedha.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 5,
+    category: "Qulqullummaa",
+    quote: "Qulqulluu ta'ee namni kadhatu afuurota hamaa ni balleessa. Maal nadhibdeedhaan namni kadhatu garuu afuuronni kan itti taphatan ta'a.",
+    author: "Qul. Eephreem"
+  },
+  {
+    id: 6,
+    category: "Qulqullummaa",
+    quote: "Ati qulqulluu waan taateef warreen ejjan irratti hin murteessiin. Kana hin ta'u taanaan atis akkuma isaa seera kan diigde taata; sababni isaas inni 'hin ejjiin' jedhe, 'hin murteessiin' jedheeraatii.",
+    author: "Abbaa Piister"
+  }
+]
