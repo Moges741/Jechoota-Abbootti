40803ebad49e49711e09d6499ee1359d64a78065
Moges741 <mogesse741@gmail.com>
2026-02-17 23:17:40 +0300

    added gaabbii quotes

---
+++ DA/app/components/Gaabbii/gaabbiiiQuotes.js
@@ -0,0 +1,63 @@
+
+export const gaabbiiQuotes = [
+  {
+    id: 1,
+    category: "Gaabbii",
+    quote: "Cubbuurraa fagaadhu gara ofii keettis gadi fageenyaan seeni; gara mootummaa Waaqayyotti kan si galchu yaabbannoo ni argatta.",
+    author: "Qul. Yisihaaq Isa Sooriyaa"
+  },
+  {
+    id: 2,
+    category: "Gaabbii",
+    quote: "Yeroo yeroon cubbuukee kan hin yaadneefi gaabbii kan hin galle yoo ta'e, guyyaawwan sana akka jiraattetti hin lakkaa'iin.",
+    author: "Qul. Yisihaaq Isa Sooriyaa"
+  },
+  {
+    id: 3,
+    category: "Gaabbii",
+    quote: "Abbaa gaabbii jechuun isa yeroo ilaaltu Waaqayyo fi ajaja isaa nama itti yaadattudha.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 4,
+    category: "Gaabbii",
+    quote: "Waa'ee cubbuutiif gaabbiifi imimmaaniin kan akka of qeequu waanti barbaadamu hin jiru.",
+    author: "Qul. Yohannis Afaan Warqee"
+  },
+  {
+    id: 5,
+    category: "Gaabbii",
+    quote: "Cubbuu isaa namni hin dubbanne hiikama cubbuu hin argatu.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 6,
+    category: "Gaabbii",
+    quote: "Cubbuu hojjechuu dhiisuun waan gaariidha. Erga hojjettee kaatee gaabbiirraa osoo hin fagaatiin gaariidha. Gaabbii erga galtee booda cubbuutti osoo hin deebitu ta'ee wayya. Cubbuutti hin deebine taanaan kana kan siif godhe Waaqayyoon galateeffachuun gaariidha.",
+    author: "Qul. Baasiliyoos"
+  },
+  {
+    id: 7,
+    category: "Gaabbii",
+    quote: "Gaabbiin Waaqayyoon sodaachuun, amantaa fi lapheetiin kan argamtu kennaa lammaffaati.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 8,
+    category: "Gaabbii",
+    quote: "Abbaa gaabbiikee waliin yeroo taatu gaaffiikee malee murteekee itti hin himiin; wanta lapheekeetti yaadde waliin akka walsimu yaadarratti yaadaafi gorsa isaanii akka siif kennan taasisi.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 9,
+    category: "Gaabbii",
+    quote: "Cubbuu isaa hubatee cubbamaan gaabbii galu cubbuu kan hin qabne garuu ofii isaa akka qulqulluutti kan of ilaaluurra ni caala.",
+    author: "Abbaa Saarmiitaas"
+  },
+  {
+    id: 10,
+    category: "Gaabbii",
+    quote: "Waaqayyo ajaa'iba waan hin raawwanneef, icciitii dhokata waan hin hubanneef yookaan barnoota garaagaraa waan hin baranneef nu hin komatu; garuu cubbuu keenyaatiif bo'uufi gaabbii galuu dhiisuu keenyaan nu komata.",
+    author: "Abbaa Waqriisan"
+  }
+]
