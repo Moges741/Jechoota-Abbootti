6fe956e777aa2376756ca16734e833672efeb2a9
Moges741 <mogesse741@gmail.com>
2026-02-17 20:09:55 +0300

    added faith quotes

---
+++ DA/app/components/Amantaa/amantaaQuoes.js
@@ -0,0 +1,34 @@
+// composables/amantaaQuotes.js
+
+export const amantaaQuotes = [
+  {
+    id: 1,
+    category: "Amantaa",
+    quote: "Amantaa jechuun qaamolee miira keenya cufnee ija sammuu keenyaan ilaaluu eegaluudha.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 2,
+    category: "Amantaa",
+    quote: "Nuti hundumti keenya Waaqni nama ta'uu fayyisaan keenya Iyyesuus Kiristoos jecha foon uffate ta'uu ni amanna. Inni waaqummaa isaatiin nama ta'uu isaafi gonka ta'uu isaa ni amanna. Waaqummaan isaa namummaa isarraa takkaallee akka adda hin baane ni amanna. Nama ta'uun isaas Waaqummaa waliin osoo hin makamiin, hin jijjiiramiin fi osoo adda hin ba'iinidha.",
+    author: "Qul. Qeerloos"
+  },
+  {
+    id: 3,
+    category: "Amantaa",
+    quote: "Waa'ee amantaa isaa namni osoo hin beekiin jiraatu Mana Gooftaasaa kan hin beekne bineelda manaatii gadi bineensatti fakkeeffama.",
+    author: "Beektota M/Amantaa"
+  },
+  {
+    id: 4,
+    category: "Amantaa",
+    quote: "Namni amantaa malee hojiitiin qofa qulqullaa'uun isaaf hin malu. Dursee amanuudhaan Abbaa hundumaa kan ta'e akka Abraham amantaa fi hojii bakka tokkotti cimsee qabuun mucaa isaa aarsaaf dhiyeesse akkuma jedhu.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 5,
+    category: "Amantaa",
+    quote: "Amantaan galaana kutee nama ceesisa; gammoojjii gidduu kan jiru dhagaarraa bishaan burqisiisee nama obaasa. Kanaafi kitaabni qulqulluun 'nama amaneef hundumtuu ni danda'ama' kan jedhu.",
+    author: "Abbaa Shinoodaa"
+  }
+]
