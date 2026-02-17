6af50afd92c1504b237ff6b337f44885ba7a4ca1
Moges741 <mogesse741@gmail.com>
2026-02-17 20:15:44 +0300

    added self-descipline quotes

---
+++ DA/app/components/OfBituu/ofBituuQoutes.js
@@ -0,0 +1,63 @@
+
+export const ofBituuQuotes = [
+  {
+    id: 1,
+    category: "Of Bituu",
+    quote: "Ati ofiikeetii waliin yoo araaramte samiifi lafti sitti ni araaramu.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 2,
+    category: "Of Bituu",
+    quote: "Laphee ofii to'achuun ofiitiin Waaqayyoof jedhanii fedhii ofiirraa fagaachuudha.",
+    author: "Abbaa Agaaton"
+  },
+  {
+    id: 3,
+    category: "Of Bituu",
+    quote: "Qulqullummaa keetitti hin amaniin; waan darbeefis hin dhiphatiin. Kanarra afaan keefi garaa keeti biti.",
+    author: "Abbaa Raa'iisaayyaas"
+  },
+  {
+    id: 4,
+    category: "Of Bituu",
+    quote: "Namni fedhii isaa hin moo'anne kan Waaqayyoo ta'uu hin danda'u. Namafi Waaqayyoo waliin kan isa wal-dhabsiisu jiraachuu fedhii isaati.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 5,
+    category: "Of Bituu",
+    quote: "Fedhii foonii moo'achuun gonfoo jireenya kan argamsiisu wareegamummaadha.",
+    author: "Qul. Yisihaaq"
+  },
+  {
+    id: 6,
+    category: "Of Bituu",
+    quote: "Barsiisuun gaariidha; kanarrammoo qulqullummaan of eeggachuun ni caala.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 7,
+    category: "Of Bituu",
+    quote: "Namni beekaa jedhamee kan galateeffatamu dhuma addunyaatti kan isa kabachiisu 'cubbuun na gahe' yeroo jedhudha.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 8,
+    category: "Of Bituu",
+    quote: "Gaddi guyyaa guyyaan taasifamu lubbuun yeroo hunda Waaqayyoon akka yaaddu taasisa.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 9,
+    category: "Of Bituu",
+    quote: "Fedhii foonii isaa kan lubbuuf hin bichisiisne, fedhiin lubbuu isaas Waaqayyoof hin bitamu.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 10,
+    category: "Of Bituu",
+    quote: "Baay'ee nyaachuufi dhuguun fedhii hamaa nutti baay'isa.",
+    author: "Maar Yisihaaq"
+  }
+]
