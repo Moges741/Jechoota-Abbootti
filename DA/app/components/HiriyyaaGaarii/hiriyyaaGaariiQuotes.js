1860e81943f649a078691f70b40030484be69b8b
Moges741 <mogesse741@gmail.com>
2026-02-17 20:21:30 +0300

    added nice friend quotes

---
+++ DA/app/components/HiriyyaaGaarii/hiriyyaaGaariiQuotes.js
@@ -0,0 +1,44 @@
+export const hiriyyaaGaariiQuotes = [
+  {
+    id: 1,
+    category: "Hiriyyaa Gaarii",
+    quote: "Hiriyyoonni kee kuma ta'uu danda'u; icciitikee dhoksee kan qabatu garuu tokko qofa dha.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 2,
+    category: "Hiriyyaa Gaarii",
+    quote: "Namoonni hundumtuu ati sirriidha jedhanii yoo amanan illee, dhugasaa dubbatee dogongorakee kan siif ibsu garuu inni hiriyyaa keeti.",
+    author: "Beekaa Tokko"
+  },
+  {
+    id: 3,
+    category: "Hiriyyaa Gaarii",
+    quote: "Hiriyyaa gaarii waliin yoo taate jireenya afuuraa keetiin gara fuulduraatti imalta; garuu hiriyyaa badaa waliin yoo taate ni kufta yookaan tarkaanfii tokko gara duubatti deebita.",
+    author: "Abboota Durii"
+  },
+  {
+    id: 4,
+    category: "Hiriyyaa Gaarii",
+    quote: "Eenyummaakee akkan sitti himu yoo barbaadde, hiriyyaakee natti himi.",
+    author: "Abbaa Shinoodaa"
+  },
+  {
+    id: 5,
+    category: "Hiriyyaa Gaarii",
+    quote: "Bakka dadhabbiikee nama sitti himu hiriyyaa gaarii taasisii qabadhu.",
+    author: "Yohannis Afaan Warqee"
+  },
+  {
+    id: 6,
+    category: "Hiriyyaa Gaarii",
+    quote: "Ejjummaan akka hin jiraanne namoota ejjan irraa fagaadhu.",
+    author: "Maar Yisihaaq"
+  },
+  {
+    id: 7,
+    category: "Hiriyyaa Gaarii",
+    quote: "Hewaan ishiin duraa jecha Bofaan dhageessee ajajaan ala ta'uu fi du'a ulfoofte. Obboleessa isaa kan ajjeesu Qaa'eliinis ni deesse. Durbeen Maariyaam garuu jecha ergamaa ifaa Qulqulluu Gabri'el missiraachoo isaa dhageessee ajajamtuu taate ('Akka jecha keetii naaf haa ta'u' jette), jireenya kan ta'e Kiristoosiin ulfoofte. Uummata isaatiif kan du'u Iyyesuus Kiristoosiin deesse.",
+    author: "Wareegamaa Qul. Yoosxiinos"
+  }
+]
